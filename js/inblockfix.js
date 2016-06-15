f5712159c4a8cbf5b92d4c34621df1c02b2d60fe
Petrlev <levendeevp@gmail.com>
2016-06-15 19:29:48 +0300

    right block new fix

--- js/inblockfix.js
+++ js/inblockfix.js
@@ -1,33 +1,85 @@
-(function(){  // анонимная функция (function(){ })(), чтобы переменные "a" и "b" не стали глобальными
-var a = document.querySelector('#aside1'), b = null;  // селектор блока, который нужно закрепить
+(function(){
+    var a = document.querySelector('#aside1'), b = null, K = null, Z = 0, P = 0, N = 0;  // если у P ноль заменить на число, то блок будет прилипать до того, как верхний край окна браузера дойдёт до верхнего края элемента, если у N — нижний край дойдёт до нижнего края элемента. Может быть отрицательным числом
 window.addEventListener('scroll', Ascroll, false);
-document.body.addEventListener('scroll', Ascroll, false);  // если у html и body высота равна 100%
+document.body.addEventListener('scroll', Ascroll, false);
 function Ascroll() {
-  if (b == null) {  // добавить потомка-обёртку, чтобы убрать зависимость с соседями
-    var Sa = getComputedStyle(a, ''), s = '';
-    for (var i = 0; i < Sa.length; i++) {  // перечислить стили CSS, которые нужно скопировать с родителя
-      if (Sa[i].indexOf('overflow') == 0 || Sa[i].indexOf('padding') == 0 || Sa[i].indexOf('border') == 0 || Sa[i].indexOf('outline') == 0 || Sa[i].indexOf('box-shadow') == 0 || Sa[i].indexOf('background') == 0) {
-        s += Sa[i] + ': ' +Sa.getPropertyValue(Sa[i]) + '; '
+  var Ra = a.getBoundingClientRect(),
+      R1bottom = document.querySelector('#article1').getBoundingClientRect().bottom;
+  if (Ra.bottom < R1bottom) {
+    if (b == null) {
+      var Sa = getComputedStyle(a, ''), s = '';
+      for (var i = 0; i < Sa.length; i++) {
+        if (Sa[i].indexOf('overflow') == 0 || Sa[i].indexOf('padding') == 0 || Sa[i].indexOf('border') == 0 || Sa[i].indexOf('outline') == 0 || Sa[i].indexOf('box-shadow') == 0 || Sa[i].indexOf('background') == 0) {
+          s += Sa[i] + ': ' +Sa.getPropertyValue(Sa[i]) + '; '
+        }
+      }
+      b = document.createElement('div');
+      b.className = "stop1";
+      b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
+      a.insertBefore(b, a.firstChild);
+      var l = a.childNodes.length;
+      for (var i = 1; i < l; i++) {
+        b.appendChild(a.childNodes[1]);
+      }
+      a.style.height = b.getBoundingClientRect().height + 'px';
+      a.style.padding = '0';
+      a.style.border = '0';
+    }
+    var Rb = b.getBoundingClientRect(),
+        Rh = Ra.top + Rb.height,
+        W = document.documentElement.clientHeight,
+        R1 = Math.round(Rh - R1bottom),
+        R2 = Math.round(Rh - W);
+    if (Rb.height > W) {
+      if (Ra.top < K) {  // скролл вниз
+        if (R2 + N > R1) {  // не дойти до низа
+          if (Rb.bottom - W + N <= 0) {  // подцепиться
+            b.className = 'sticky';
+            b.style.top = W - Rb.height - N + 'px';
+            Z = N + Ra.top + Rb.height - W;
+          } else {
+            b.className = 'stop1';
+            b.style.top = - Z + 'px';
+          }
+        } else {
+          b.className = 'stop1';
+          b.style.top = - R1 +'px';
+          Z = R1;
+        }
+      } else {  // скролл вверх
+        if (Ra.top - P < 0) {  // не дойти до верха
+          if (Rb.top - P >= 0) {  // подцепиться
+            b.className = 'sticky';
+            b.style.top = P + 'px';
+            Z = Ra.top - P;
+          } else {
+            b.className = 'stop1';
+            b.style.top = - Z + 'px';
+          }
+        } else {
+          b.className = '';
+          b.style.top = '';
+          Z = 0;
+        }
+      }
+      K = Ra.top;
+    } else {
+      if ((Ra.top - P) <= 0) {
+        if ((Ra.top - P) <= R1) {
+          b.className = 'stop1';
+          b.style.top = - R1 +'px';
+        } else {
+          b.className = 'sticky';
+          b.style.top = P + 'px';
+        }
+      } else {
+        b.className = '';
+        b.style.top = '';
       }
     }
-    b = document.createElement('div');  // создать потомка
-    b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
-    a.insertBefore(b, a.firstChild);  // поместить потомка в цепляющийся блок первым
-    var l = a.childNodes.length;
-    for (var i = 1; i < l; i++) {  // переместить во вновь созданного потомка всех остальных потомков (итого: создан потомок-обёртка, внутри которого по прежнему работают скрипты)
-      b.appendChild(a.childNodes[1]);
-    }
-    a.style.height = b.getBoundingClientRect().height + 'px';  // если под скользящим элементом есть другие блоки, можно своё значение
-    a.style.padding = '0';
-    a.style.border = '0';  // если элементу присвоен padding или border
+    window.addEventListener('resize', function() {
+      a.children[0].style.width = getComputedStyle(a, '').width
+    }, false);
   }
-  if (a.getBoundingClientRect().top <= 0) { // elem.getBoundingClientRect() возвращает в px координаты элемента относительно верхнего левого угла области просмотра окна браузера
-    b.className = 'sticky';
-  } else {
-    b.className = '';
-  }
-  window.addEventListener('resize', function() {
-    a.children[0].style.width = getComputedStyle(a, '').width
-  }, false);  // если изменить размер окна браузера, измениться ширина элемента
 }
 })()
